12e0beebfd8f78141853897beefd41862e0428fa
merykul <mariakulv@gmail.com>
2023-02-07 20:16:30 +0300

    created test

---
+++ src/test/java/UpdatePet.java
@@ -0,0 +1,19 @@
+import api.clients.PetClient;
+import models.PetRequest;
+import models.PetResponse;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class UpdatePet {
+    @Test
+    public void updatePet() {
+        PetClient client = new PetClient();
+        PetRequest requestBody = new PetRequest(
+                "Patron",
+                "available");
+        PetResponse response = client.updatePet(requestBody);
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(response.getName(), "Patron");
+        softAssert.assertEquals(response.getStatus(), "available");
+    }
+}
